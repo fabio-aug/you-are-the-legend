4c1d86e2769cf34be89547505a32f86b99665ac1
Fábio Augusto <fabio.augusto1911@gmail.com>
2022-01-26 09:36:10 -0300

    Finalizada criação da classe MOB

--- src/classes/Mob.java
+++ src/classes/Mob.java
@@ -1,5 +1,40 @@
 package classes;
 
 public class Mob {
-    private String nome;
+    private String name;
+    private String description;
+    private int force;
+    private int life;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
 }
