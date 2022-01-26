aa5c79082d158fbcc836672f4f876c087048078d
Rodrigo Cardoso <rodrigo.cardoso1007@gmail.com>
2022-01-26 09:39:40 -0300

    Creating class Item

---
+++ src/classes/Item.java
@@ -0,0 +1,31 @@
+packge class;
+
+public class Item {
+    private String name;
+    private String description;
+    private int damage;
+    private int vitality;
+
+    public Item(String name, String description, int damege, int vitality) {
+        this.name = name,
+        this.description = description,
+        this.damege = damege = damege,
+        this.vitality = vitality,
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getVitality() {
+        return vitality;
+    }
+}
