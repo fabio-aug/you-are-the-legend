cf7128db3d83761ebb8788a1c1964358e14e8105
Fábio Augusto <fabio.augusto1911@gmail.com>
2022-01-26 09:36:59 -0300

    Criado construtor da classe MOB

--- src/classes/Mob.java
+++ src/classes/Mob.java
@@ -6,6 +6,13 @@ public class Mob {
     private int force;
     private int life;
 
+    public Mob(String name, String description, int force, int life) {
+        this.name = name;
+        this.description = description;
+        this.force = force;
+        this.life = life;
+    }
+
     public String getName() {
         return name;
     }
